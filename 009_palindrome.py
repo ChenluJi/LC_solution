ebe32efc7cd470c614f7d3c6adb5014dc4bbb6d8
Chenlu Ji <cji@control-tec.com>
2018-11-19 17:03:45 -0500

    java solution

---
+++ 009_palindrome.py
@@ -0,0 +1,28 @@
+'''
+Determine whether an integer is a palindrome. Do this without extra space.
+
+click to show spoilers.
+
+Some hints:
+
+Could negative integers be palindromes? (ie, -1)
+
+If you are thinking of converting the integer to string, note the restriction of using extra space.
+
+You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+
+There is a more generic way of solving this problem.
+'''
+
+class Solution():
+   def is_palidrome(self, num):
+    pass
+
+    
+if __name__ == '__main__':
+    # a = Solution()
+    # print(a.convert('1000000003'))
+    # b = Solution2()
+    # print(b.reverse_integer(1000000003))
+    print(len(123))
+   
